0b15c3109c1f31be02ce60a118bc859de2475917
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-02-21 13:02:16 +0530

    adding prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json']
-  },
-  plugins: ['react'],
-  rules: {}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'prettier',
+        'plugin:react/jsx-runtime',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'comma-dangle': 0,
+        'arrow-body-style': 0,
+        'prefer-arrow-callback': 0,
+    },
 }
